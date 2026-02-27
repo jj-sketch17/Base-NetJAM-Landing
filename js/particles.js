a185f18bb84cbf9d57a78867653430f23605ba99
jj-sketch17 <elchucupei01@gmail.com>
2026-02-27 17:03:11 -0400

    fix:Correccion de colores para toda la pagina

--- js/particles.js
+++ js/particles.js
@@ -1,14 +1,14 @@
 particlesJS("particles-js", {
   "particles": {
     "number": { "value": 100, "density": { "enable": true, "value_area": 800 } },
-    "color": { "value": "#00C2FF" },
+    "color": { "value": "#ff0000" },
     "shape": { "type": "circle" },
     "opacity": { "value": 0.5 },
     "size": { "value": 3, "random": true }, // Nodos pequeños
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#00C2FF",
+      "color": "#f6ff00",
       "opacity": 0.4,
       "width": 1
     },
